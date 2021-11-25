4a174dd4e4eeb25f11f387670c7f861d76f5a3a9
André Marques <andre.marques@edu.pucrs.br>
2021-11-25 19:44:56 -0300

    remove comments in main

--- src/main/kotlin/MainKt.kt
+++ src/main/kotlin/MainKt.kt
@@ -1,14 +1,7 @@
-import kotlin.system.exitProcess
-
 class MainKt {
     companion object{
         @JvmStatic fun main(args: Array<String>) {
-//            if (args.size != 1) {
-//                println("Run: java -jar https-simulator.jar")
-//                exitProcess(1)
-//            }
             HTTPS().simulate()
-
         }
     }
 }
